61dff973a2f7656c2615c3f94bfd53987b276f4e
Minjung Sung <minjungsung1994@gmail.com>
2022-04-24 14:07:37 +0000

    tasks

---
+++ tasks/_build/test/lib/tasks/ebin/tasks.app
@@ -0,0 +1,6 @@
+{application,tasks,
+             [{applications,[kernel,stdlib,elixir,logger]},
+              {description,"tasks"},
+              {modules,['Elixir.Tasks']},
+              {registered,[]},
+              {vsn,"0.1.0"}]}.
---
+++ tasks/lib/tasks/application.ex
@@ -0,0 +1,18 @@
+defmodule Tasks.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      {MyApp.MyTask, 123}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
+end
